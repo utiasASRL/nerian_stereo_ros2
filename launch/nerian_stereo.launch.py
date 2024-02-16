04f6f61befb92c9564ffa4688c38650ff8490dc4
a-krawciw <freakyfactoid@gmail.com>
2024-02-16 17:29:25 -0500

    Update IP address

--- launch/nerian_stereo.launch.py
+++ launch/nerian_stereo.launch.py
@@ -6,8 +6,9 @@ def generate_launch_description():
     nerian_stereo_node = Node(
                 package='nerian_stereo',
                 executable='nerian_stereo_node',
+                prefix='xterm -e gdb -ex run --args',
                 parameters=[
-                    {'remote_host':                  '192.168.132.10'},
+                    {'remote_host':                  '192.168.131.10'},
                     {'remote_port':                  '7681'},
                     {'use_tcp':                       True},
 
